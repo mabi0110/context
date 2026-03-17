1f9df1a413f9dddb67236ada73c4f831265537a7
mabi0110 <mariadb0110gmail.com>
2026-03-17 14:55:39 +0100

    display all Objects

--- src/main/java/pl/javastart/context/ContextApplication.java
+++ src/main/java/pl/javastart/context/ContextApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class ContextApplication {
 
@@ -11,6 +13,10 @@ public class ContextApplication {
         ConfigurableApplicationContext context = SpringApplication.run(ContextApplication.class, args);
         MessagePrinter messagePrinter = context.getBean(MessagePrinter.class);
         messagePrinter.printMessage();
+
+        Arrays.stream(context.getBeanDefinitionNames())
+                .map(context::getBean)
+                .forEach(System.out::println);
     }
 
 }
